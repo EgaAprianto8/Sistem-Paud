7da180b1e89cb6de086c8e459a81a970bed866ed
EgaAprianto <egaaprianto04@gmail.com>
2024-06-05 11:03:27 +0700

    [ADD] - Table

---
+++ app/dashboard/progress/page.tsx
@@ -0,0 +1,12 @@
+import IndikatorNilai from '@/components/pages/dashboard/IndikatorNilai/IndikatorNilai'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <IndikatorNilai/>
+    </div>
+  )
+}
+
+export default page
